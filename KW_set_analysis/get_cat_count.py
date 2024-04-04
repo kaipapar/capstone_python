21d2ff3a060eb13f7a925ca367bb754e662f3832
kaipapar <120021905+kaipapar@users.noreply.github.com>
2024-04-04 19:58:31 +0300

    Made script OOP style for funzies
    - class for storing variables and methods

--- KW_set_analysis/get_cat_count.py
+++ KW_set_analysis/get_cat_count.py
@@ -1,30 +1,53 @@
 import pandas as pd
 from sklearn.datasets import load_iris
-import math
+import numpy as np
 
-kw_list = pd.read_excel("KW_list_full.xlsx", usecols='A:B', sheet_name='KW_list') #get kw list here
-fin_data = pd.read_excel("KW_list_full.xlsx", usecols='A:AU', sheet_name='Fin_data')
-""" eng_data = pd.read_excel("KW_list_full.xlsx", usecols='', sheet_name='Eng_data')
-old_data = pd.read_excel("KW_list_full.xlsx", usecols='', sheet_name='Old_data') """
-results = pd.read_excel("KW_list_full.xlsx", usecols='A:H', sheet_name='Results', skiprows=3)
-""" keywords_columns = [f'Keyword {i}' for i in range(1, 21)]
-column_data = results[[f'{i}' for i in range(0,6)]] """
+class Category_counter():
+    
+    def __init__(self, data, file_in, file_out):
+        self.data = data
+        self.file_in = file_in
+        self.file_out = file_out
+        self.kw_list = pd.read_excel(file_in, usecols='A:B', sheet_name='KW_list') #get kw list here
+        self.results = pd.DataFrame()
 
-print(kw_list.head())
-print(fin_data.columns)
-print(results.head())
-""" One execution per row of thesis data """
-def add_counts(row):
-    for value in row:
-        selected = kw_list[kw_list['Keywords'] == value]
-        if not selected.empty:
-            #print(selected)
-            results.at[row.name, selected.iloc[0,1]] += 1
+    def read(self):
+        print(self.kw_list.head())
+        print(self.data.head())
+        self.results = pd.DataFrame(0, index=range(len(self.data)), columns=range(8))
+        self.results[0] = self.data.iloc[:, 0].values
 
+        print(self.data.head())
+        print(self.results.head())
+        """ One execution per row of thesis data """
 
-fin_data.apply(add_counts, axis=1)
+        self.data.apply(self.add_counts, axis=1)
+        print(self.results.head())
 
-print(results.head())
+    def add_counts(self, row):
+        for value in row:
+            selected = self.kw_list[self.kw_list['Keywords'] == value]
+            if not selected.empty:
+                self.results.at[row.name, selected.iloc[0,1]] += 1
+
+    def write_xcel(self):
+        # creating an ExcelWriter object
+        with pd.ExcelWriter(self.file_out) as writer:
+            # writing to the 'Employee' sheet
+            self.results.to_excel(writer, index=False)
+        #print('DataFrames are written to Excel File successfully.')
+
+if __name__ == "__main__":
+    file = "KW_list_full.xlsx"
+    file_out = "KW_occurence_count.xlsx"
+    #data = pd.read_excel(file, usecols='A:AU', sheet_name='Fin_data') 
+    data = pd.read_excel(file, usecols='', sheet_name='Eng_data')
+    #data = pd.read_excel(file, usecols='', sheet_name='Old_data')
+    print(data.head())
+    cls_instance = Category_counter(data, file, file_out)
+    cls_instance.read()
+    cls_instance.write_xcel()
+
 #kws=pd.DataFrame(add_keywords())
 """ def add_keywords(csv_1, kws):
     for index, row in csv_1.loc[:, [keywords_columns]].iterrows():
